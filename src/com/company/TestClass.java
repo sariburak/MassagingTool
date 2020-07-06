cf1b3c805a337306915e1c14e445e059570e1aa7
sariburak <43667154+sariburak@users.noreply.github.com>
2020-07-06 10:08:08 +0300

    testing commit

--- src/com/company/TestClass.java
+++ src/com/company/TestClass.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("My time!");
+        System.out.println("My time has probably not come yet!");
     }
 }
 
