a00d22bfbd39bf8991fe9c19d224e698b9c3cb10
sariburak <43667154+sariburak@users.noreply.github.com>
2020-07-06 10:03:53 +0300

    testing commit

--- src/com/company/TestClass.java
+++ src/com/company/TestClass.java
@@ -7,21 +7,7 @@ import java.util.regex.Pattern;
 
 public class TestClass {
     public static void main(String[] args) {
-        Scanner myScn = new Scanner(System.in);
-        String input = myScn.nextLine();
-
-
-        ArrayList<String> finalString = new ArrayList<String>();
-        Pattern p = Pattern.compile("\"(?:[^\"\\\\]|\\\\.)*\"|\\S+");
-        Matcher m = p.matcher(input);
-
-        while (m.find()) {
-            finalString.add(m.group(0));
-        }
-
-        for(String pattern : finalString){
-            System.out.println(pattern + "with len: " + pattern.length());
-        }
+        System.out.println("My time!");
     }
 }
 
