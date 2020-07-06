337380fff3d573ddfcd50b3ea2453b16a2e2c2fd
sariburak <43667154+sariburak@users.noreply.github.com>
2020-07-06 13:41:18 +0300

    input testing on Client

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -23,24 +23,33 @@ public class Client {
     }
 
     private void Run() {
-        String input = "";
+        String input;
         String reply;
         Scanner scn = new Scanner(System.in);
 
         while(true){
             input = scn.nextLine();
 
+            //Check if exit
+            if(input.toLowerCase().equals("exit")) {
+                try {
+                    dos.writeUTF("exit");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Disconnecting...");
+                break;
+            }
+
             //Send input
             try {
+                //Is this gonna writeUTF even if throws exception?
                 dos.writeUTF(parseInput(input));
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-
-            //Check if exit
-            if(input.toLowerCase().equals("exit")) {
-                System.out.println("Disconnecting...");
-                break;
+            } catch (InvalidInputFormatException e) {
+                System.out.println(e.getMessage());
+                continue;
             }
 
             //Receive reply (assumes that every input will get a reply)
@@ -55,15 +64,53 @@ public class Client {
         disconnectClient();
     }
 
-    private String parseInput(String input) {
-        ArrayList<String> finalString = new ArrayList<String>();
+    private void testInput(ArrayList<String> finalString) throws InvalidInputFormatException {
+        finalString.set(0, finalString.get(0).toUpperCase());
+
+        switch (finalString.get(0)) {
+            case "LOGOUT":
+            case "INBOX":
+            case "OUTBOX":
+            case "LISTUSERS":
+                if(finalString.size() != 1){
+                    throw new InvalidInputFormatException();
+                }
+                break;
+            case "LOGIN":
+            case "SEND":
+                if (finalString.size() != 3) {
+                    throw new InvalidInputFormatException();
+                }
+                break;
+            case "CREATEUSER":
+                if (finalString.size() != 8) {
+                    throw new InvalidInputFormatException();
+                }
+                break;
+            case "REMOVEUSER":
+                if (finalString.size() != 2) {
+                    throw new InvalidInputFormatException();
+                }
+                break;
+            default:
+                throw new InvalidInputFormatException();
+        }
+    }
+
+    private String parseInput(String input) throws InvalidInputFormatException {
+        ArrayList<String> finalString = new ArrayList<>();
         Pattern p = Pattern.compile("\"(?:[^\"\\\\]|\\\\.)*\"|\\S+");
         Matcher m = p.matcher(input);
 
         while (m.find()) {
             finalString.add(m.group(0));
         }
-        System.out.println(String.join(pipe, finalString));
+        //To uppercase
+        finalString.set(0, finalString.get(0).toUpperCase());
+        //Test for invalidity
+        testInput(finalString);
+
+        //Return protocol
         return String.join(pipe, finalString);
     }
 
@@ -88,5 +135,15 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+}
+
+class InvalidInputFormatException extends Exception{
+    public InvalidInputFormatException(){
+        super("Invalid input format");
+    }
+
+    public InvalidInputFormatException(String message){
+        super(message);
     }
 }
